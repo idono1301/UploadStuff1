dda71accc81b2d1d15ec511ef9d70c6ce2ec46a6
Geoffrey <idono1301@hotmail.com>
2012-12-09 18:59:25 -1000

    Working rspec thanks to Jonpro

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -18,12 +18,17 @@ describe "TestLogins" do
    end
    describe "as an admin" do
       it "should see student listing" do
-         user = User.create(:password => "password", :password_confirmation => 'password', :email => 'sample@sample.com')
-         user.save!
+         @admin = User.create do |u|
+            u.email = 'test@test.com'
+            u.password = 'password'
+            u.password_confirmation = 'password'
+            u.admin = true
+         end
+         @admin.save!
          visit '/'
          click_link_or_button("Sign In")
-         fill_in 'Email', :with => user.email
-         fill_in 'Password', :with => user.password
+         fill_in 'Email', :with => @admin.email
+         fill_in 'Password', :with => @admin.password
          click_link_or_button("Sign in")
          page.should have_content "Signed in successfully."
          click_link_or_button("Students")
@@ -32,17 +37,21 @@ describe "TestLogins" do
    end
    describe "as an admin" do
       it "should see All Users" do
-         user = User.create(:password => 'password', :password_confirmation => 'password', :email => 'sample@sample.com', :admin => true)
-         user.save!
+         @admin = User.create do |u|
+            u.email = 'test@test.com'
+            u.password = 'password'
+            u.password_confirmation = 'password'
+            u.admin = true
+         end
+         @admin.save!
          visit '/'
          click_link_or_button("Sign In")
-         fill_in 'Email', :with => user.email
-         fill_in 'Password', :with => user.password
+         fill_in 'Email', :with => @admin.email
+         fill_in 'Password', :with => @admin.password
          click_link_or_button("Sign in")
          page.should have_content "Signed in successfully."
-         #         click_link_or_button("Users")
-         visit users_path
-         page.should have_content 'All Users'
+         click_link_or_button("Users")
+         page.should have_content 'All Userss'
       end
    end
 
